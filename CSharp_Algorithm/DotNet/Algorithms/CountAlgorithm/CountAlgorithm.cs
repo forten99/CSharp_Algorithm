e244b7a36dd13fb9aab7cb6909ffea56a18ec2eb
SUN <forten99@gmail.com>
2024-06-19 22:32:24 +0900

    개수 알고리즘(Count Algorithm)

---
+++ CSharp_Algorithm/DotNet/Algorithms/CountAlgorithm/CountAlgorithm.cs
@@ -0,0 +1,37 @@
+﻿// [?] 1부터 1000까지의 정수 중 13의 배수의 개수(건수, 횟수)
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 개수 알고리즘(Count Algorithm): 주어진 범위에 주어진 조건에 해당하는 자료들의 개수
+/// </summary>
+class CountAlgorithm
+{
+    static void Main()
+    {
+        // [1] Input: 1부터 1,000가지의 데이터 
+        var numbers = Enumerable.Range(1, 1_000).ToArray(); // _ 언더스코어를 사용하여 콤마처럼 숫자 표기 가능
+        int count = 0;
+
+        // [2] Process : 개수 알고리즘 영역: 주어진 범위에 주어진 조건(필터링)
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] % 13 == 0)
+            {
+                // count = count + 1;
+                // count += 1;
+                count++;    // COUNT
+            }
+        }
+
+        // [3] Output
+        Console.WriteLine($"1부터 1000까지의 정수 중 13의 배수의 개수: {count}");
+
+        // Expression
+        //Enumerable.Range(1, 1000).ToArray().Where(n => n % 13 == 0).Count()
+        //Enumerable.Range(1, 1000).ToArray().Count(n => n % 13 == 0)
+    }
+}
--- CSharp_Algorithm/DotNet/Algorithms/SumAlgorithm.cs
+++ CSharp_Algorithm/DotNet/Algorithms/SumAlgorithm.cs
@@ -1,33 +1,33 @@
-﻿////[?] n명의 국어 점수 중에서 80점 이상인 접수의 합계
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿//[?] n명의 국어 점수 중에서 80점 이상인 접수의 합계
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-///// <summary>
-///// 합계 알고리즘(Sum Algorithm) : 주어진 범위에 주어진 조건에 해당하는 자료들의 합계
-///// </summary>
-//class SumAlgorithm
-//{
-//    static void Main()
-//    {
-//        // [1] Input : n명의 국어 점수
-//        int[] scores = { 100, 75, 50, 37, 90, 95 };
-//        int sum = 0;
-        
-//        // [2] Process : 합계 알고리즘 영역: 주어진 범위에 주어진 조건(필터링)
-//        for(int i=0; i<scores.Length; i++) 
-//        {
-//            if (scores[i] >= 80)
-//            {
-//                sum += scores[i];   // SUM
-//            }
-//        }
+/// <summary>
+/// 합계 알고리즘(Sum Algorithm) : 주어진 범위에 주어진 조건에 해당하는 자료들의 합계
+/// </summary>
+class SumAlgorithm
+{
+    static void Main()
+    {
+        // [1] Input : n명의 국어 점수
+        int[] scores = { 100, 75, 50, 37, 90, 95 };
+        int sum = 0;
 
-//        // [3] Output
-//        Console.WriteLine($"{scores.Length}명의 점수 중 80점 이상의 총점: {sum}");
+        // [2] Process : 합계 알고리즘 영역: 주어진 범위에 주어진 조건(필터링)
+        for (int i = 0; i < scores.Length; i++)
+        {
+            if (scores[i] >= 80)
+            {
+                sum += scores[i];   // SUM
+            }
+        }
 
-//        // [!] 디버거(디버깅 S/W)를 사용하여 디버깅하기: F9 -> F5 -> F11 -> F5
-//    }
-//}
+        // [3] Output
+        Console.WriteLine($"{scores.Length}명의 점수 중 80점 이상의 총점: {sum}");
+
+        // [!] 디버거(디버깅 S/W)를 사용하여 디버깅하기: F9 -> F5 -> F11 -> F5
+    }
+}
