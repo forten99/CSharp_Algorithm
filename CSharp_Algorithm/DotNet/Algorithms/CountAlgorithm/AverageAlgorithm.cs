244c922fb44927823aa8ed3227cb55d51f192550
SUN <forten99@gmail.com>
2024-06-22 11:48:22 +0900

    평균 알고리즘(Average Algorithm)

---
+++ CSharp_Algorithm/DotNet/Algorithms/CountAlgorithm/AverageAlgorithm.cs
@@ -0,0 +1,34 @@
+﻿//[?] n명의 점수 중에서 80점 이상 95점 이하인 점수의 평균
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 평균 알고리즘(Average Algorithm): 주어진 범위에 주어진 조건에 해당하는 자료들의 평균
+class AverageAlgorithm
+{
+    static void Main()
+    {
+        //[1] 입력: n명의 성적
+        int[] data = { 90, 65, 78, 50, 95 };
+        //data.Where(d => d >= 80 && d<=95).Average()
+        int sum = 0;    // 합계 담는 그릇
+        int count = 0;  // 개수 담는 그릇
+
+        //[2] 처리: AVG = SUM / COUNT
+        for (int i = 0; i < data.Length; i++)
+        {
+            if (data[i] >=80 && data[i] <=95)
+            {
+                sum += data[i]; // SUM
+                count++;        // COUNT
+            }
+        }
+        double avg = sum / (double)count;   // AVERAGE
+
+        //[3] 출력
+        Console.WriteLine($"80점 이상 95점 이하인 자료의 평균: {avg}"); // 92.5
+
+    }
+}
