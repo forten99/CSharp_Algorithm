67997ae6e7af0ad83db3c0f30cdf99333c02f01a
SUN <forten99@gmail.com>
2024-06-23 21:49:37 +0900

    알고리즘 최댓값과 최솟값 제외한 평균값 구하기

---
+++ CSharp_Algorithm/DotNet/Algorithms/Average/AverageExceptMaxAndMin.cs
@@ -0,0 +1,44 @@
+﻿//[?] 최댓값과 최솟값을 제외한 나머지의 평균값을 구하는 프로그램
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+// 최댓값과 최솟값을 제외한 평균값 구하기
+class AverageExceptMaxAndMin
+{
+    static void Main()
+    {
+        //[1] Input
+        int[] scores = { 10, 20, 30, 40, 50 };  // 30
+        var sum = 0;                            // SUM
+        var max = Int32.MinValue;               // 해당 범위에서 가장 작은 값으로 초기화
+        var min = Int32.MaxValue;               // 해당 범위에서 가장 큰 값으로 초기화
+        var avg = 0.0;                          // AVERAGE
+
+        //[2] Process
+        for (int i = 0; i < scores.Length; i++)
+        {
+            sum += scores[i];       // SUM
+            if (scores[i] > max)
+            {
+                max = scores[i];    // MAX
+            }
+            if (scores[i] < min)
+            {
+                min = scores[i];    // MIN
+            }
+        }
+
+        //[!] 최댓값과 최솟값을 제외한 평균값 구하기
+        avg = (sum - max - min) / (double)(scores.Length - 2);
+
+        //[3] Output
+        Console.WriteLine($"합계: {sum}, 최댓값: {max}, 최솟값: {min}");
+        Console.WriteLine($"최댓값과 최솟값을 제외한 평균값: {avg}");
+        Console.WriteLine($"최댓값과 최솟값을 제외한 평균값(식): {scores.Where(n => n!= scores.Max() || n!= scores.Min()).Average()}");
+
+    }
+}
--- CSharp_Algorithm/DotNet/Algorithms/MinAlgorithm/MinAlgorithm.cs
+++ CSharp_Algorithm/DotNet/Algorithms/MinAlgorithm/MinAlgorithm.cs
@@ -11,11 +11,11 @@ class MinAlgorithm
     static void Main()
     {
         //[1] Initialize
-        int min = int.MaxValue; // 정수 형식의 데이터 중 가장 큰 값으로 초기화
+        int min = Int32.MaxValue; // 정수 형식의 데이터 중 가장 큰 값으로 초기화
 
-        //[2] Input
+        //[2] Input: 이진수로 표현 + 숫자 구분자
         //int[] numbers = { 2, 5, 3, 7, 1 };
-        int[] numbers = { 0b0010, 0B_0101, 0b0011, 0B_0111, 0b0000_0001 };  // 이진수로 표현 + 숫자 구분자
+        int[] numbers = { 0b0010, 0B_0101, 0b0011, 0B_0111, 0b0000_0001 };  
 
         //[3] Process : MIN
         for (int i = 0; i < numbers.Length; i++)
